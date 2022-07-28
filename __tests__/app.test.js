6c9cbf7c9b83e00dc9a2bfcb1dec25b7fed2c097
Chad Stabler <stablerpsn@gmail.com>
2022-07-27 19:26:57 -0500

    Game function working for getAll

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,24 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const request = require('supertest');
-const app = require('../lib/app');
+// const request = require('supertest');
+// const app = require('../lib/app');
+const Game = require('../lib/models/Game');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('getAll should return a list of game titles with ids', async () => {
+    const resp = await Game.getAll();
+    expect(resp.length).toEqual(10);
+    expect(resp[0]).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      release_year: expect.any(Number),
+      genre: expect.any(String),
+      main_developer: expect.any(String),
+      open_world: expect.any(Boolean)
+    });
   });
   afterAll(() => {
     pool.end();
